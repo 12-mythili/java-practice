6691915155d35f1f53dcf7108240d00cdaeab71a
unknown <mythilimanoharan366@gmail.com>
2025-06-17 18:18:22 +0530

    added new file

---
+++ print1100.java
@@ -0,0 +1,11 @@
+class print1100
+{
+    public static void main(String[] args)
+    {
+        int i;
+        for(i=1;i<=100;i++)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
