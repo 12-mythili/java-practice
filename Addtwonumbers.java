6d44521a383ef8cf8e03b541fbe7c38ba1317882
12-mythili <155502771+12-mythili@users.noreply.github.com>
2025-06-17 15:44:21 +0530

    Create Addtwonumbers.java

---
+++ Addtwonumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int num1,num2,sum;
+        num1=scan.nextInt();
+        num2=scan.nextInt();
+        sum=num1+num2;
+        System.out.print(sum);
+    }
+}
