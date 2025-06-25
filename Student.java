fcef318e02a657703ad53d5de54b6a0bfa0dc506
unknown <mythilimanoharan366@gmail.com>
2025-06-25 19:29:31 +0530

    student details

--- Student.java
+++ Student.java
@@ -1,26 +1,68 @@
 import java.util.Scanner;
-public class Student{
+public class Student
+{
     String name;
-    int roll_no;
+    int regno;
     String dept;
-    int mobile_no;
-    public Student(){
-        Scanner scanner=new Scanner(System.in);
-        System.out.print("Enter name: ");
-        this.name=scanner.nextLine();
-        System.out.print("Enter roll_no: ");
-        this.roll_no=scanner.nextInt();
-        System.out.print("Enter dept: ");
-        this.dept=scanner.nextLine();
-        scanner.nextLine();
-        System.out.print("Enter mobile_no: ");
-        this.mobile_no=scanner.nextInt();
- }
+    int mobile;
+    public Student()
+    {
+    }
+    public void setname(String name)
+    {
+        this.name=name;
+    }
+    public void setregno(int regno)
+    {
+        this.regno=regno;
+    }
+    public void setdept(String dept)
+    {
+        this.dept=dept;
+    }
+    public void setmobile(int mobile)
+    {
+        this.mobile=mobile;
+    }
+    public String getname()
+    {
+        return name;
+    }
+    public int getregno()
+    {
+        return regno;
+    }
+    public String getdept()
+    {
+        return dept;
+    }
+    public long getmobile()
+    {
+        return mobile;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Student s = new Student();
 
-    public static void main(String[] args){
-    Student student= new Student();
+        System.out.println("Enter the below student details");
+        System.out.print("Enter the student name: ");
+        s.setname(scan.nextLine());
+        System.out.print("Enter the student register no: ");
+        s.setregno(scan.nextInt());
+        scan.nextLine();
+        System.out.print("Enter the student department: ");
+        s.setdept(scan.nextLine());
+        System.out.print("Enter the student mobile : ");
+        s.setmobile(scan.nextInt());
+        scan.nextLine();
+
+        System.out.println("Student name: " + s.getname());
+        System.out.println("Student register: " + s.getregno());
+        System.out.println("Student department: " + s.getdept());
+        System.out.println("Student mobile: " + s.getmobile());
+    }
+    
 }
 
-}
 
 
