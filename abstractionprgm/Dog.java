c267a198015a27e458edbb5396fb4878f06a3d53
unknown <mythilimanoharan366@gmail.com>
2025-07-14 16:06:21 +0530

    Dog class is added

---
+++ abstractionprgm/Dog.java
@@ -0,0 +1,17 @@
+package abstractionprgm;
+import java.util.Scanner;
+
+public class Dog extends Animal{
+    String dogsound;
+    public void setSound()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter the dog sound :");
+        dogsound=scan.nextLine();
+    }
+    public void getSound()
+    {
+        System.out.println("the dog sound is :"+dogsound);
+    }
+    
+}
